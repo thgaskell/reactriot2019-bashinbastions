4ccdd25ac587d8dc64e6627841a79f0e0b758016
Emmanuel Pilande <epilande@gmail.com>
2019-09-27 18:53:04 -0700

    :tiger: Add meow to parse args

--- cli.js
+++ cli.js
@@ -1,5 +1,37 @@
 #!/usr/bin/env node
 
+const meow = require("meow");
 const { renderApp } = require("./dist/index");
+
+const cli = meow(
+  `
+  Usage:
+    $ TBD [options]
+
+  Options:
+    --help, -h      Display this message.
+    --version, -v   CLI Version.
+
+  Examples:
+    $ TBD
+`,
+  {
+    flags: {
+      help: {
+        type: "boolean",
+        alias: "h",
+      },
+      version: {
+        type: "boolean",
+        alias: "v",
+      },
+    },
+  },
+);
+
+const { input, flags } = cli;
+
+console.log("input: ", input);
+console.log("flags: ", flags);
 
 renderApp();
