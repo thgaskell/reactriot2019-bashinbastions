8eec6876e151583215d91e57e361cc0aa0c7d3c2
Tony Gaskell <thgaskell@gmail.com>
2019-09-27 19:10:08 -1000

    Add functionality to read an ssh config file

---
+++ src/utils/config.spec.ts
@@ -0,0 +1,47 @@
+import { readConfigurationFile, getHosts } from "./config";
+
+test(`should try to load the default config file`, async () => {
+  expect(
+    readConfigurationFile("config/path/that/does/not/exist"),
+  ).rejects.toThrow(
+    `ENOENT: no such file or directory, open '/Users/thgaskell/git/reactriot2019-bashinbastions/config/path/that/does/not/exist'`,
+  );
+});
+
+test(`parse config file`, () => {
+  const singleConfigContents = `
+IdentityFile ~/.ssh/id_rsa
+
+Host host-1
+  Hostname host-1.example.com
+  User user1
+  Port 22
+  ForwardAgent yes
+  IdentityFile ~/.ssh/id_rsa
+
+Host host-2
+  Hostname host-2.example.com
+  User user2
+  Port 22
+  ForwardAgent yes
+  IdentityFile ~/.ssh/id_rsa
+`;
+  expect(getHosts(singleConfigContents)).toEqual([
+    {
+      host: "host-1",
+      hostname: "host-1.example.com",
+      user: "user1",
+      port: "22",
+      forwardAgent: "yes",
+      identityFile: "~/.ssh/id_rsa",
+    },
+    {
+      host: "host-2",
+      hostname: "host-2.example.com",
+      user: "user2",
+      port: "22",
+      forwardAgent: "yes",
+      identityFile: "~/.ssh/id_rsa",
+    },
+  ]);
+});
