aa7d7057c5cc6e1e8043d30afda0d795455b8444
Emmanuel Pilande <epilande@gmail.com>
2019-09-28 00:41:40 -0700

    :page_with_curl: Display list of hosts

--- src/commands/list.tsx
+++ src/commands/list.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { Box } from "ink";
+import { Box, Color } from "ink";
+import { readConfigurationFile, getHosts, Host } from "../utils/config";
 
 export const List: React.FunctionComponent = () => {
-  const data = [{ value: "a" }, { value: "b" }, { value: "c" }];
+  const [hosts, setHosts] = React.useState<Host[]>([]);
+
+  React.useEffect(() => {
+    async function fetchConfiguration() {
+      const configurationFile = (await readConfigurationFile()).toString();
+      setHosts(getHosts(configurationFile as any));
+    }
+    fetchConfiguration();
+  }, []);
 
   return (
     <Box flexDirection="column">
-      {data.map(d => (
-        <Box key={d.value}> - {d.value}</Box>
+      {hosts.map(host => (
+        <React.Fragment key={host.hostname}>
+          <Box>
+            -{" "}
+            <Color bold green>
+              {host.host}
+            </Color>
+          </Box>
+          <Box marginLeft={4} flexDirection="column">
+            <Box>
+              <Color gray>Hostname:</Color> {host.hostname}
+            </Box>
+            <Box>
+              <Color gray>User:</Color> {host.user}
+            </Box>
+            <Box>
+              <Color gray>Port:</Color> {host.port}
+            </Box>
+            <Box>
+              <Color gray>ForwardAgent:</Color> {host.forwardAgent}
+            </Box>
+            <Box>
+              <Color gray>IdentityFile:</Color> {host.identityFile}
+            </Box>
+          </Box>
+        </React.Fragment>
       ))}
     </Box>
   );
