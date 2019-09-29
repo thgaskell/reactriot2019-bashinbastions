c92ee6dcec20f6d490d0c826573ba6345e1cbd2a
Emmanuel Pilande <epilande@gmail.com>
2019-09-28 23:28:15 -0700

    :beetle: Fix config init triggering twice

--- src/commands/init.tsx
+++ src/commands/init.tsx
@@ -5,39 +5,46 @@ import SshConfig from "../lib/SshConfig";
 const USER_HOME_SSH_CONFIG_FILEPATH = "~/.ssh/config";
 
 export const Init: React.FunctionComponent = () => {
-  const [isLoading, setLoading] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(true);
   const [config, setConfig] = React.useState<SshConfig>();
   const [error, setError] = React.useState<Error | null>(null);
 
-  // TODO: Fix this. It's triggering twice.
-  if (!isLoading && !config) {
-    setLoading(true);
-    SshConfig.init()
-      .then(async config => {
-        await config.import(USER_HOME_SSH_CONFIG_FILEPATH);
-        setLoading(false);
-        setConfig(config);
-      })
-      .catch(err => {
-        setLoading(false);
-        setError(err);
-        process.exit(1);
-      });
-  }
+  React.useEffect(() => {
+    async function initConfig() {
+      if (!config) {
+        SshConfig.init()
+          .then(async config => {
+            await config.import(USER_HOME_SSH_CONFIG_FILEPATH);
+            setLoading(false);
+            setConfig(config);
+          })
+          .catch(err => {
+            setLoading(false);
+            setError(err);
+            process.exit(1);
+          });
+      }
+    }
+    initConfig();
+  }, []);
 
   return (
-    <React.Fragment>
-      <Box>
-        Importing user ssh config{" "}
-        <Color yellow>({USER_HOME_SSH_CONFIG_FILEPATH})</Color>
-      </Box>
+    <Box flexDirection="column">
+      {isLoading && (
+        <Box>
+          Importing user ssh config{" "}
+          <Color yellow>({USER_HOME_SSH_CONFIG_FILEPATH})</Color>
+        </Box>
+      )}
 
       {config && (
         <React.Fragment>
           <Box>
             Initialized <Color yellow>{config.filepath}</Color>
           </Box>
-          <Color green>Setup complete!</Color>
+          <Box>
+            <Color green>Setup complete!</Color>
+          </Box>
         </React.Fragment>
       )}
 
@@ -46,6 +53,6 @@ export const Init: React.FunctionComponent = () => {
           <Color red>Error</Color>: {error.message}
         </Box>
       )}
-    </React.Fragment>
+    </Box>
   );
 };
