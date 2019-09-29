6d53bb5084a365d44e1d2a321243415804832fd1
Emmanuel Pilande <epilande@gmail.com>
2019-09-29 00:13:55 -0700

    :hammer: Stub out connect/disconnect selection

--- src/commands/disconnect.tsx
+++ src/commands/disconnect.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { Box, Text } from "ink";
+import SelectInput from "ink-select-input";
+import { useHosts } from "../hooks/useHosts";
 
 export const Disconnect: React.FunctionComponent = () => {
-  const data = [{ value: "a" }, { value: "b" }, { value: "c" }];
+  const hosts = useHosts();
+  const items = hosts.map(({ Host }) => ({
+    label: Host,
+    value: Host,
+  }));
+
+  if (!items.length) return null;
 
   return (
     <Box flexDirection="column">
-      <Text>Disconnect</Text>
-      {data.map(d => (
-        <Box key={d.value}> - {d.value}</Box>
-      ))}
+      <Box>
+        <Text bold>Close Host connection:</Text>
+      </Box>
+      <SelectInput
+        items={items}
+        onSelect={item => {
+          console.log("Connect -> item", item);
+          process.exit(0);
+        }}
+      />
     </Box>
   );
 };
