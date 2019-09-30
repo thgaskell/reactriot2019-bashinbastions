331e677c06b99980ff04cad0348fcf43624ccf0f
Emmanuel Pilande <epilande@gmail.com>
2019-09-29 23:22:50 -0700

    :information_source: Update --help message

--- cli.js
+++ cli.js
@@ -6,14 +6,27 @@ const { renderApp } = require("./dist/index");
 const cli = meow(
   `
   Usage:
-    $ TBD [options]
+    $ rssh [command] [options]
+
+  Commands:
+    init            Initialize the cli working directory.
+    create host     Add hosts to your SSH config.
+    create tunnel   Add tunnels to your SSH config.
+    list            Lists all hosts.
+    show <host>     Show a specific host's information.
+    connect         Connect to a configured tunnel.
+    disconnect      Kill a tunnel connection that was created through the CLI.
 
   Options:
     --help, -h      Display this message.
     --version, -v   CLI Version.
 
   Examples:
-    $ TBD
+    $ rssh init
+    $ rssh create host
+    $ rssh create tunnel
+    $ rssh connect
+    $ rssh disconnect
 `,
   {
     flags: {
